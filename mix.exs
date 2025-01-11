1bd1245553c2c1176a06de1118720543dbabbf07
c2bw <59066181+c2bw@users.noreply.github.com>
2025-01-11 21:07:07 +0100

    chore: bump package version to 1.45.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DockerEngineAPI.Mixfile do
   def project do
     [
       app: :elixir_dea,
-      version: "1.45.5",
+      version: "1.45.6",
       elixir: "~> 1.18",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
